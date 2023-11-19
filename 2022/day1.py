e16b05351f96d9798e6035baf254b21fcdd9c0aa
bjrtx <57685415+bjrtx@users.noreply.github.com>
2023-11-19 23:36:51 +0100

    refactor 2022/01

--- 2022/day1.py
+++ 2022/day1.py
@@ -4,8 +4,11 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(2022, 1)
 data = puzzle.input_data.splitlines()
-groups = (g for k, g in itertools.groupby(data, key=lambda s: s != "") if k)
-calories = (sum(int(x) for x in g) for g in groups)
+calories = (
+    sum(int(x) for x in g)
+    for k, g in itertools.groupby(data, key=lambda s: s != "")
+    if k
+)
 top_three = heapq.nlargest(3, calories)
 puzzle.answer_a = top_three[0]
 puzzle.answer_b = sum(top_three)
