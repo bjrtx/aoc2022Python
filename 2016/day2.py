864a1174312067736a88fdfb20718fe0b6368a33
bjrtx <57685415+bjrtx@users.noreply.github.com>
2023-12-08 12:58:20 +0100

    solve 2023/06

--- 2016/day2.py
+++ 2016/day2.py
@@ -5,7 +5,7 @@ from aocd.models import Puzzle
 puzzle = Puzzle(year=2017, day=2)
 data = [[int(x) for x in line.split()] for line in puzzle.input_data.splitlines()]
 
-puzzle.answer_a = sum(max(l) - min(l) for l in data)
+puzzle.answer_a = sum(max(line) - min(line) for line in data)
 
 divs = [[divmod(b, a) for a, b in itertools.combinations(sorted(line), 2)] for line in data]
 puzzle.answer_b = sum(next(q for q, r in d if r == 0) for d in divs)
--- 2023/day6.py
+++ 2023/day6.py
@@ -1,10 +1,25 @@
-import functools
-import itertools
-
-import more_itertools
-
-import tqdm
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2023, day=6)
-data = puzzle.input_data
+data = puzzle.input_data.splitlines()
+
+times = [int(x) for x in data[0].partition(":")[-1].split()]
+distances = [int(x) for x in data[1].partition(":")[-1].split()]
+
+res = 1
+for t, d in zip(times, distances):
+    minimum = min(s for s in range(t) if s * (t - s) >= d)
+    maximum = max(s for s in range(t) if s * (t - s) >= d)
+    res *= (maximum - minimum + 1)
+
+puzzle.answer_a = res
+
+
+time = int(''.join(data[0].partition(":")[-1].split()))
+distance = int(''.join(data[1].partition(":")[-1].split()))
+
+speed = (distance // time)
+minimum = min(s for s in range(speed, time) if s * (time - s) >= distance)
+maximum = max(s for s in range(speed, time) if s * (time - s) >= distance)
+
+puzzle.answer_b = maximum - minimum + 1
