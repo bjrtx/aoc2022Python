7b0cb44a5933fe4d2c09ebe185c3bf1c4ca37d40
bjrtx <57685415+bjrtx@users.noreply.github.com>
2023-12-08 20:32:32 +0100

    solve 2015/15

---
+++ 2015/day15.py
@@ -0,0 +1,32 @@
+import itertools
+import math
+import re
+
+import numpy as np
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2015, day=15)
+data = puzzle.input_data.splitlines()
+
+ingredients = np.array([[int(m[0]) for m in re.finditer(r'-?\d+', line)] for line in data])
+
+
+def simplex(n):
+    for p in itertools.product(range(101), repeat=n-1):
+        if sum(p) <= 100:
+            yield *p, 100 - sum(p)
+
+
+def scores(count_calories):
+    ingredients_no_calories = ingredients[:, :-1]
+    if count_calories:
+        calories = ingredients[:, -1]
+    print(ingredients_no_calories)
+    for p in simplex(len(ingredients_no_calories)):
+        if not count_calories or np.dot(p, calories) == 500:
+            total = np.maximum(p @ ingredients_no_calories, 0)
+            yield math.prod(total)
+
+
+puzzle.answer_a = max(scores(count_calories=False))
+puzzle.answer_b = max(scores(count_calories=True))
