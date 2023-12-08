c83325a7a8e19370eb7420455cae4da8133cae0f
bjrtx <57685415+bjrtx@users.noreply.github.com>
2023-12-08 19:07:36 +0100

    misc linting

--- 2015/day1.py
+++ 2015/day1.py
@@ -1,3 +1,6 @@
+import itertools
+
+import more_itertools
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2015, day=1)
@@ -5,9 +8,5 @@ data = puzzle.input_data.strip()
 
 puzzle.answer_a = 2 * data.count('(') - len(data)
 
-pos = 0
-for i, c in enumerate(data, start=1):
-    pos += 1 if c == '(' else -1
-    if pos == -1:
-        break
-puzzle.answer_b = i
+positions = itertools.accumulate(1 if c == '(' else -1 for c in data)
+puzzle.answer_b = next(more_itertools.iter_index(positions, -1)) + 1
--- 2015/day2.py
+++ 2015/day2.py
@@ -1,16 +1,15 @@
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2015, day=2)
-res = 0
+data = [sorted(int(x) for x in line.split('x')) for line in puzzle.input_data.splitlines()]
 
-for line in puzzle.input_data.splitlines():
-    l, w, h = map(int, line.split('x'))
+res = 0
+for l, w, h in data:
     sides = (l * w, l * h, w * h)
     res += min(sides) + 2 * sum(sides)
 puzzle.answer_a = res
 
 res = 0
-for line in puzzle.input_data.splitlines():
-    l, w, h = sorted(map(int, line.split('x')))
+for l, w, h in data:
     res += 2 * (l + w) + l * w * h
 puzzle.answer_b = res
