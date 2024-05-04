6854bcef770148fd989ea3a63fbd2fcf644fe778
bjrtx <57685415+bjrtx@users.noreply.github.com>
2024-05-04 19:49:41 +0200

    Solve 2016/01-02

--- 2016/day02.py
+++ 2016/day02.py
@@ -14,16 +14,38 @@ def positions():
             v = more_itertools.ilen(g)
             match c:
                 case 'U':
-                    row -= v
+                    row = max(0, row - v)
                 case 'D':
-                    row += v
+                    row = min(2, row + v)
                 case 'L':
-                    col -= v
+                    col = max(0, col - v)
                 case 'R':
-                    col += v
-            row = min(max(0, row), 2)
-            col = min(max(0, col), 2)
+                    col = min(2, col + v)
         yield row * 3 + col + 1
 
 
 puzzle.answer_a = ''.join(map(str, positions()))
+
+
+def positions2():
+    layout = [
+        'xx1xx', 'x234x', '56789', 'xABCx', 'xxDxx'
+    ]
+    row, col = 0, -2
+    for w in data:
+        for c, g in itertools.groupby(w):
+            v = more_itertools.ilen(g)
+            match c:
+                case 'U':
+                    row = max(row - v, abs(col) - 2)
+                case 'D':
+                    row = min(row + v, 2 - abs(col))
+                case 'L':
+                    col = max(col - v, abs(row) - 2)
+                case 'R':
+                    col = min(col + v, 2 - abs(row))
+
+        yield layout[row + 2][col + 2]
+
+
+puzzle.answer_b = ''.join(map(str, positions2()))
