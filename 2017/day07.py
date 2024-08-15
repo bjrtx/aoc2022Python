315f652093b8c1c5713226ebf2cb0b90999a3f40
bjrtx <57685415+bjrtx@users.noreply.github.com>
2024-08-15 10:42:38 +0200

    Solve 2017/07b

--- 2017/day07.py
+++ 2017/day07.py
@@ -1,5 +1,7 @@
+import collections
 import itertools
 import re
+from functools import cache
 
 import more_itertools
 from aocd.models import Puzzle
@@ -8,12 +10,31 @@ puzzle = Puzzle(year=2017, day=7)
 data = puzzle.input_data.splitlines()
 
 nodes = {}
+parent = {}
 for line in data:
     name, _, out = line.partition('->')
     out = out.strip().split(', ') if out else ''
-    print(name, out)
     name, weight = re.match(r'(\w+) \((\d+)\)', name).groups()
-    nodes[name] = (weight, out)
+    nodes[name] = (int(weight), out)
+    parent.update((n, name) for n in out)
+
 
 have_incoming = set(itertools.chain.from_iterable(out for _, out in nodes.values()))
-puzzle.answer_a = more_itertools.one(set(nodes.keys()) - have_incoming)
+puzzle.answer_a = more_itertools.one(set(nodes) - have_incoming)
+
+
+@cache
+def tower_weight(node):
+    weight, out = nodes[node]
+    return weight + sum(tower_weight(n) for n in out)
+
+
+for node, (_, out) in nodes.items():
+    c = collections.Counter(tower_weight(n) for n in out)
+    if len(out) > 1 and 1 in c.values():
+        tw = next(k for k, v in c.items() if v == 1)
+        defect = next(n for n in out if tower_weight(n) == tw)
+        other_tw = next(k for k, v in c.items() if v != 1)
+        w, _ = nodes[defect]
+        puzzle.answer_b = other_tw - tw + w
+        break
