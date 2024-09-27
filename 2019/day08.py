df04f7691a1b0b72973de88eaea42eff959301ad
bjrtx <57685415+bjrtx@users.noreply.github.com>
2024-09-27 12:13:58 +0200

    Solve 2019/08

---
+++ 2019/day08.py
@@ -0,0 +1,27 @@
+import asyncio
+import itertools
+from itertools import pairwise, permutations
+
+import more_itertools
+from more_itertools import value_chain
+from aocd.models import Puzzle
+
+from day05 import run
+
+puzzle = Puzzle(year=2019, day=8)
+data = puzzle.input_data
+
+WIDTH, HEIGHT = 25, 6
+layers = list(itertools.batched(data, WIDTH * HEIGHT))
+
+layer = min(layers, key=lambda t: t.count('0'))
+puzzle.answer_a = layer.count('1') * layer.count('2')
+
+values = [
+    next((x for x in ls if x != '2'), '2')
+    for ls in zip(*layers)
+]
+message = '\n'.join(
+    ''.join('▯' if c == '0' else '▮' for c in v)
+    for v in itertools.batched(values, WIDTH))
+print(message)
