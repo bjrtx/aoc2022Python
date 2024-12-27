6558e373763463ffae4612c3314438b2ca90e224
bjrtx <b.jartoux@gmail.com>
2024-12-27 01:37:53 +0100

    Solve 2021/03b

--- 2021/day03.py
+++ 2021/day03.py
@@ -1,16 +1,27 @@
 from aocd.models import Puzzle
+from more_itertools import one
 
 puzzle = Puzzle(year=2021, day=3)
 data = puzzle.input_data.splitlines()
 
-c = [0 for _ in data[0]]
-for s in data:
-    for i, x in enumerate(s):
-        if x == '1':
-            c[i] += 1
+c = [t.count('1') for t in zip(*data)]
 
-most_common_bits = ['1' if 2 * v >= len(data) else '0' for v in c]
-not_bits = ['0' if c == '1' else '1' for c in most_common_bits]
-gamma_rate = int(''.join(most_common_bits), 2)
-epsilon_rate = int(''.join(not_bits), 2)
+gamma_rate = 0
+for v in c:
+    gamma_rate = (gamma_rate << 1) | (2 * v >= len(data))
+epsilon_rate = gamma_rate ^ ((1 << len(data[0])) - 1)
 puzzle.answer_a = gamma_rate * epsilon_rate
+
+oxygen_candidates = data[:]
+co2_candidates = data[:]
+for i in range(len(data[0])):
+    if len(oxygen_candidates) == 1:
+        break
+    most_common = 2 * sum(x[i] == '1' for x in oxygen_candidates) >= len(oxygen_candidates)
+    oxygen_candidates = [x for x in oxygen_candidates if x[i] == '01'[most_common]]
+for i in range(len(data[0])):
+    if len(co2_candidates) == 1:
+        break
+    least_common = 2 * sum(x[i] == '0' for x in co2_candidates) > len(co2_candidates)
+    co2_candidates = [x for x in co2_candidates if x[i] == '01'[least_common]]
+puzzle.answer_b = int(one(oxygen_candidates), 2) * int(one(co2_candidates), 2)
